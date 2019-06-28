6c2b80f0af3dbe61d4c2c9230b29eadc69dcbcf6
Elena Tseller <elena.tseller@akvelon.com>
2019-06-28 14:18:38 +0300

    Login page and related functionality

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+
+class LoginPageLocators(object):
+    LOG_IN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
